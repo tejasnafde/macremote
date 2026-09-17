e8d65f0621783cbbf789021e00d3d1c28ea79e15
Tejas Nafde <nafdetejas@gmail.com>
2026-09-18 00:45:49 +0530

    Release v0.5.5

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "io.github.tejasnafde.macremote"
         minSdk = 24
         targetSdk = 36
-        versionCode = 24
-        versionName = "0.5.4"
+        versionCode = 25
+        versionName = "0.5.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
